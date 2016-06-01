8df6444855751a4c1a5593c7ffe69304b2910146
Coffeeteer <tgrieshiemer@gmail.com>
2016-06-01 14:11:04 -0400

    added more funcionality to game

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -14,34 +14,71 @@ $(document).ready(function() {
 		console.log(randCrystals);
 		return randCrystals;
 	}
-	var captureNums = genRandNumCrystals(); 
-	console.log(captureNums);
-	console.log(getRandNum);
-	var cpuRandNumber = getRandNum();
-	var randNumberCrystals =  
-	// random number displayed
-	$('#numBeat').html(cpuRandNumber);
-	var crytals = $('.crys_img');
-	console.log(crytals);
 	
-	for(var i = 0; i < crytals.length; i++){
-		var element = crytals[i];
-		$(element).attr("value", captureNums[i]);
-		console.log(crytals[i]);
+	// initializing the custom data attributes
+	// we will use them again and again
+	// we could also make 2 variable to maintain this info
+	// Actually that is far easier
+	var win = 0;
+	var loss = 0;
+	function initializeRandomNumbers(){
+    	var captureNums = genRandNumCrystals(); 
+    	console.log(captureNums);
+    	console.log(getRandNum);
+    	var cpuRandNumber = getRandNum();
+    	var randNumberCrystals =  
+    	// random number displayed
+    	$('#numBeat').html(cpuRandNumber);
+    	var crytals = $('.crys_img');
+    	console.log(crytals);
+    
+        //var total = 0;	
+    	for(var i = 0; i < crytals.length; i++){
+    		var element = crytals[i];
+    		$(element).attr("data-myRandomNumber", captureNums[i]);
+    		// <img src="assets/images/crystal-img.png" id="img1" class="crys_img">
+    		// changes to 
+    		// <img src="assets/images/crystal-img.png" data-myRandomNumber="23" id="img1" class="crys_img">
+    		//saw that !!
+    		console.log(crytals[i]);
+    	}
 	}
+	
+	initializeRandomNumbers();
 
 	var total = 0;
 
 	console.log('total', total);
 	$('.crys_img').on('click', function(){ 
 //		console.log('str')
-		
-		var playerNumber = parseInt($(this).attr("value"));
+		// we have to be consistant with our code, right !
+		var playerNumber = parseInt($(this).attr("data-myRandomNumber"));
 		console.log('playerNumber', playerNumber);
 		total += playerNumber;
 		console.log('total', total);
 		console.log($(this).attr("value"));
 		$('#pNumber').html(total);
+		if(total == cpuRandNumber){
+		    // the total is equal to cpuRandNumber
+		    // yeyee we have a winner
+		    alert("winner");
+		    win++;
+		    $("#win").html("Wins : " + win);
+		    total = 0;
+		    $('#pNumber').html(0);
+		    initializeRandomNumbers();
+		    alert("You win");
+		}else if(total > cpuRandNumber){
+		    // total got beyond cpuRandNumber
+		    // we lose
+		    // whoops , dodged a bullet
+		    loss++;
+		    $("#loss").html("Loss : " + loss);
+		    total = 0;
+		    $('#pNumber').html(0);
+		    initializeRandomNumbers();
+		    alert("You lose");
+		}
 
 	});
 
